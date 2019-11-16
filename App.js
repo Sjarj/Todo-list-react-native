ad3b149a56d2b29f0065d6d479bebbc3c816730f
Sjarj <sebastien.jarjanette@icloud.com>
2019-11-16 15:01:16 +0100

    Add a state

--- App.js
+++ App.js
@@ -1,19 +1,27 @@
-import React from "react";
-import { View } from "react-native";
-import Header from "./components/header";
-import { Button } from "react-native-elements";
-import Icon from "react-native-vector-icons/FontAwesome";
+import React from 'react';
+import { View } from 'react-native';
+import Header from './components/header';
+import { Button } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      myText: 'Un text par défaut'
+    };
+  }
+
   render() {
     return (
       <View>
-        <Header content="Liste de taches" />
+        <Header content='Liste de taches' />
         <Button
           large
-          icon={<Icon name="code" />}
+          icon={<Icon name='code' />}
           iconRight
-          title="Button with right icon"
+          title='Button with right icon'
         />
       </View>
     );
